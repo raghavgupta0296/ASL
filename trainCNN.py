4926940ff9794d5beb8b1f8c3199fe0403ff1562
Raghav Gupta <raghav0296@gmail.com>
2017-04-09 13:41:36 +0530

    Add files via upload

--- trainCNN.py
+++ trainCNN.py
@@ -2,22 +2,38 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
-im_ht = 300
-im_wid = 300
-no_labels = 26
-
-filepath = "./TrainingImages/"
-filepaths = []
-letters = [chr(i) for i in range(65,91)]
-for l in letters:
-    filepaths.append(filepath+l+"/")
+im_ht = 100
+im_wid = 100
+no_labels = 2
 
 x = []
 y = []
+
+filepath = "./TrainingImages/"
+filepaths = []
+letters = [chr(i) for i in range(65,67)] #91
+for l in letters:
+    for num in range(1,21,1):
+        filepaths.append(filepath+l+"/"+"%s.png"%str(num))
+        y.append(l)
+
 for f in filepaths:
-    im = cv2.imshow(f)
+    im = cv2.imread(f,-1)
+    im = cv2.resize(im,(100,100))
     x.append(im)
 x = np.array(x)
+y= np.array(y)
+
+def one_hot(y):
+    al2num = dict(zip(letters,range(len(letters))))
+    new_y = np.zeros(shape=(len(y),len(letters)))
+    for i in range(len(y)):
+        new_y[i][al2num[y[i]]] = 1
+    return new_y
+y = one_hot(y)
+
+x = np.reshape(x,(x.shape[0],x.shape[1],x.shape[2],1))
+print(x.shape,y.shape)
 
 X = tf.placeholder(tf.float32,shape=[None,im_ht,im_wid,1])
 Y = tf.placeholder(tf.float32,shape=[None,no_labels])
@@ -48,9 +64,9 @@ conv = tf.nn.relu(conv_2d(maxPool,W2)+b2)
 maxPool = max_pool(conv)
 
 # Dense Layer1
-W3 = ini_wt([im_ht/4*im_wid/4*64,1024])
+W3 = ini_wt([int(im_ht/4*im_wid/4*64),1024])
 b3 = ini_bias([1024])
-r = tf.reshape(maxPool,[-1,im_ht/4*im_wid/4*64])
+r = tf.reshape(maxPool,[-1,int(im_ht/4*im_wid/4*64)])
 r = tf.nn.relu(tf.matmul(r,W3)+b3)
 
 # dropout
@@ -69,10 +85,18 @@ optimizer = tf.train.AdamOptimizer().minimize(cross_entropy)
 accuracy = tf.equal(tf.argmax(r,1),tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(accuracy,tf.float32))
 
+saver = tf.train.Saver()
+best_acc = 0
+
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(20000):
+        print("Epoch : %d / 20000 "%i)
         optimizer.run(feed_dict={X:x,Y:y,dropout_keepProb:0.5})
         if i%100==0:
             print ("\n Training Acc : ")
-            print(accuracy.run(feed_dict={X:x,Y:y,dropout_keepProb:1}))
+            acc = sess.run(accuracy,feed_dict={X:x,Y:y,dropout_keepProb:1})
+            print (acc*100,"%")
+            if(acc>best_acc):
+                best_acc = acc
+                saver.save(sess,"./tfWeights") #./output/tfWeights
